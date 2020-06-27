2537347afd9f337fa4ced1f44dbc037fe2ebbbb6
Rafa Santana <rafaelsantana.ti.dev@gmail.com>
2020-06-27 03:11:22 -0300

    Adicionando movimento ao personagem

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     cenario = new Cenario(imagemCenario, 3);
     personagem = new Personagem(imagemPersonagem);
+    frameRate(40);
     //somDoJogo.loop();
 }
   
